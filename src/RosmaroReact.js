a2fb0df3cc4fc871aa6b29cc224ba58863b7b684
Łukasz Makuch <zopsesen@gmail.com>
2018-03-17 22:35:09 -0400

    the model is built just once

--- src/RosmaroReact.js
+++ src/RosmaroReact.js
@@ -6,18 +6,15 @@ class RosmaroReact extends React.Component {
     super(props);
     this.state = {
       view: null
-    }
-
-  }
-
-  refreshView() {
-    Promise.resolve(
-      rosmaro(Object.assign({}, this.props, {
-        afterTransition: () => {
-          if (this.props.afterTransition) this.props.afterTransition();
-          this.refreshView();
-        }
-      })).render()).then(view => this.setState({ view }))
+    };
+    this.refreshView = () => Promise.resolve(this.model.render())
+      .then(view => this.setState({view}));
+    this.model = rosmaro(Object.assign({}, this.props, {
+      afterTransition: () => {
+        if (this.props.afterTransition) this.props.afterTransition();
+        this.refreshView();
+      }
+    }));
   }
 
   componentDidMount() {
